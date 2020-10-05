368c6fdd2cd69eda6f18fd36bb248c7049bc9427
Amaninder Singh <amandeolofficial@gmail.com>
2020-10-05 18:17:37 -0400

    disabled the google map api
    it's not needed anymore on the website

--- js/main.js
+++ js/main.js
@@ -246,24 +246,24 @@ var $container, $blog_container;
 			check out the link below for more information about api usage
 			https://developers.google.com/maps/documentation/javascript/examples/marker-simple
 		*/
-		function initializeMap() {
-			if($('.map').length) {
-				var mapCanvas = $('#map-canvas');
-				var myLatlng = new google.maps.LatLng(mapCanvas.data("latitude"),mapCanvas.data("longitude"));
-				var mapOptions = {
-					zoom: mapCanvas.data("zoom"),
-					center: myLatlng
-				}
-				var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+		// function initializeMap() {
+		// 	if($('.map').length) {
+		// 		var mapCanvas = $('#map-canvas');
+		// 		var myLatlng = new google.maps.LatLng(mapCanvas.data("latitude"),mapCanvas.data("longitude"));
+		// 		var mapOptions = {
+		// 			zoom: mapCanvas.data("zoom"),
+		// 			center: myLatlng
+		// 		}
+		// 		var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 				
-				var marker = new google.maps.Marker({
-				  position: myLatlng,
-				  map: map
-		  	});
-			}
+		// 		var marker = new google.maps.Marker({
+		// 		  position: myLatlng,
+		// 		  map: map
+		//   	});
+		// 	}
 		  
-		}
-		google.maps.event.addDomListener(window, 'load', initializeMap);
+		// }
+		// google.maps.event.addDomListener(window, 'load', initializeMap);
 		// ------------------------------
 		
 		
