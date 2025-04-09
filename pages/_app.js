707ac0ee1314cba7abca0c527f9e0dd21a8d9638
tigermuffin10 <ktestmodule@gmail.com>
2025-04-09 08:04:15 -0400

    updated post css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import "@/styles/CGslSOa9btCG.css";
+import "@/styles/styles.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
